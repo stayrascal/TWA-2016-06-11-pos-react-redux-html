7c6cb31d0bc0eda76abcff516e371b2919eaf9c7
rascal <stayrascal@gmail.com>
2016-06-12 13:19:13 +0800

    add server,js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var path = require('path');
-
+var webpack = require('webpack');
 var components_dir = path.resolve(__dirname, 'components');
 var build_dir = path.resolve(__dirname, 'public');
 
@@ -7,8 +7,13 @@ module.exports = {
     entry: components_dir + '/index.js',
     output: {
         path: build_dir,
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/static/'
     },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [
             {
